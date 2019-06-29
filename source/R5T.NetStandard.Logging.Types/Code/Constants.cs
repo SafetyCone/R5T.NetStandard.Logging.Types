f5580765cc1c6bd33b79bab6cb157c596aeb3787
MinexAutomation <minex.automation@gmail.com>
2019-06-29 11:34:50 -0400

    Renamed ConstantsBasic to Constants and moved to the Types namespace.

--- source/R5T.NetStandard.Logging.Types/Code/Constants.cs
+++ source/R5T.NetStandard.Logging.Types/Code/Constants.cs
@@ -3,9 +3,9 @@
 using Microsoft.Extensions.Logging;
 
 
-namespace R5T.NetStandard.Logging
+namespace R5T.NetStandard.Logging.Types
 {
-    public static class ConstantsBasic
+    public static class Constants
     {
         public const LogLevel DefaultMinimumLogLevel = LogLevel.Information;
     }
