9116f7a53844b3f3d47a0ed2e4e24bd93d3c4bdc
MinexAutomation <minex.automation@gmail.com>
2019-06-29 11:31:50 -0400

    Added ConstantsBasic functionality.

---
+++ source/R5T.NetStandard.Logging.Types/Code/ConstantsBasic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Microsoft.Extensions.Logging;
+
+
+namespace R5T.NetStandard.Logging
+{
+    public static class ConstantsBasic
+    {
+        public const LogLevel DefaultMinimumLogLevel = LogLevel.Information;
+    }
+}
